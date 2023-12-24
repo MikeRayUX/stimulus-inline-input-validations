01f80296e695f45b12e75fb0d4e5d51a61fa2923
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2023-12-24 06:21:10 -0500

    Add testing library and initial test for existing validations

---
+++ cypress/e2e/single_validations/numericality.cy.js
@@ -0,0 +1,42 @@
+describe("single_valiations/numericality", () => {
+  beforeEach(() => {
+    cy.visit("http://127.0.0.1:8080/");
+  });
+
+  it(`renders error div on blank input field with data-validate-numericality attribute set to true`, () => {
+    cy.get(`input[data-validate-numericality="true"][data-field='currency']`)
+      .type("abc")
+
+    cy.get(
+      'div[data-input-validator-target="errors"][data-field="currency"]'
+    ).within(() => {
+      cy.get("div").should("exist").should("contain", "Must be a number");
+    });
+  });
+
+  it(`renders error div on blank input field with data-validate-numericality attribute set to true`, () => {
+    cy.get(`input[data-validate-numericality="true"][data-field='currency']`)
+      .invoke("attr", "data-validate-numericality", "")
+
+    cy.get(`input[data-validate-numericality][data-field='currency']`)
+      .type("abc")
+
+    cy.get(
+      'div[data-input-validator-target="errors"][data-field="currency"]'
+    ).within(() => {
+      cy.get("div").should("exist").should("contain", "Must be a number");
+    });
+  });
+
+  it(`doesn't render error on valid number`, () => {
+    cy.get(`input[data-validate-numericality="true"][data-field='currency']`)
+      .type("123")
+
+    cy.get(
+      'div[data-input-validator-target="errors"][data-field="currency"]'
+    ).within(() => {
+      cy.get("div").should("not.exist");
+    });
+  });
+
+});
