4bb460eda33316862ac9ef4f292c8b41c16e502c
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2023-12-25 06:09:16 -0500

    Add a demo playback spec for playback in visual browser mode

---
+++ cypress/e2e/demo_playback/demo.cy.js
@@ -0,0 +1,92 @@
+describe("demo_playback", () => {
+  const TYPING_DELAY_MS = 100;
+  const FOCUS_FIELD_DELAY_MS = 500;
+  const SHORT_DELAY_MS = 500;
+  const SHOW_OFF_DELAY_MS = 850;
+
+  it(`showcases validations functionality`, () => {
+    cy.visit("http://127.0.0.1:8080/");
+
+    // Presence START
+    cy.get(
+      `input[data-validate-presence="true"][data-field='fullName']`
+    ).click();
+    cy.wait(FOCUS_FIELD_DELAY_MS);
+
+    cy.get(`input[data-validate-presence="true"][data-field='fullName']`)
+      .type("sample text", { delay: TYPING_DELAY_MS })
+      .clear();
+    cy.wait(SHOW_OFF_DELAY_MS);
+    // Presence END
+
+    // Length START
+    cy.get(`input[data-validate-length="5,10"][data-field='userName']`).click();
+    cy.wait(FOCUS_FIELD_DELAY_MS);
+
+    cy.get(`input[data-validate-length="5,10"][data-field='userName']`).type(
+      "abcd",
+      { delay: TYPING_DELAY_MS }
+    );
+    cy.wait(SHOW_OFF_DELAY_MS);
+
+    cy.get(`input[data-validate-length="5,10"][data-field='userName']`).clear();
+    cy.wait(SHORT_DELAY_MS);
+
+    cy.get(`input[data-validate-length="5,10"][data-field='userName']`).type(
+      "longer text",
+      { delay: TYPING_DELAY_MS }
+    );
+    cy.wait(SHOW_OFF_DELAY_MS);
+    // Length END
+
+    // Numericality START
+    cy.get(`input[data-validate-numericality="true"][data-field='currency']`).click();
+    cy.wait(FOCUS_FIELD_DELAY_MS);
+
+    cy.get(
+      `input[data-validate-numericality="true"][data-field='currency']`
+    ).type("12345", { delay: TYPING_DELAY_MS });
+    cy.wait(SHOW_OFF_DELAY_MS);
+
+    cy.get(
+      `input[data-validate-numericality="true"][data-field='currency']`
+    ).clear();
+    cy.wait(SHORT_DELAY_MS);
+
+    cy.get(
+      `input[data-validate-numericality="true"][data-field='currency']`
+    ).type("abcdefg", { delay: TYPING_DELAY_MS });
+    cy.wait(SHOW_OFF_DELAY_MS);
+    // Numericality END
+
+    // Email START
+    cy.get(`input[data-validate-email="true"][data-field='emailField']`).click();
+    cy.wait(FOCUS_FIELD_DELAY_MS);
+
+    cy.get(
+      `input[data-validate-email="true"][data-field='emailField']`
+    ).type("example.test@example.com", { delay: 0 });
+    cy.wait(SHOW_OFF_DELAY_MS);
+
+    cy.get(
+      `input[data-validate-email="true"][data-field='emailField']`
+    ).clear();
+    cy.wait(SHORT_DELAY_MS);
+
+    cy.get(
+      `input[data-validate-email="true"][data-field='emailField']`
+    ).type("12345", { delay: 0 });
+    cy.wait(SHOW_OFF_DELAY_MS);
+    // Email END
+
+    // Multiple validations START
+    cy.get(`input[data-field='multiple']`).click();
+    cy.wait(FOCUS_FIELD_DELAY_MS);
+
+    cy.get(
+      `input[data-field='multiple']`
+    ).type("abc", { delay: TYPING_DELAY_MS });
+    cy.wait(SHOW_OFF_DELAY_MS);
+    // Multiple validations END
+  });
+});
