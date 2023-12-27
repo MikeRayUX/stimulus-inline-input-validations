c8b611d88f6c916cbc660e162b4aa462ef09468d
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2023-12-26 23:10:48 -0500

    Handle custom error styles/css

--- cypress/e2e/all_specs.cy.js
+++ cypress/e2e/all_specs.cy.js
@@ -1,11 +1,14 @@
-// import "./events/input/presence.cy";
-// import "./events/input/length.cy";
-// import "./events/input/numericality.cy";
-// import "./events/input/email.cy";
-// import "./events/input/multiple_validations.cy";
+import "./events/input/presence.cy";
+import "./events/input/length.cy";
+import "./events/input/numericality.cy";
+import "./events/input/email.cy";
+import "./events/input/multiple_validations.cy";
 
-// import "./events/blur/presence.cy";
-// import "./events/blur/length.cy";
-// import "./events/blur/numericality.cy";
-// import "./events/blur/email.cy";
-// import "./events/blur/multiple_validations.cy";
+import "./events/blur/presence.cy";
+import "./events/blur/length.cy";
+import "./events/blur/numericality.cy";
+import "./events/blur/email.cy";
+import "./events/blur/multiple_validations.cy";
+
+import "./styles/css.cy";
+import "./styles/classes.cy";
---
+++ cypress/e2e/styles/classes.cy.js
@@ -0,0 +1,25 @@
+describe("styles/classes", () => {
+  beforeEach(() => {
+    cy.visit("http://127.0.0.1:8080/");
+  });
+
+  it(`It applys default css styles if none are passed as data-errors-styles-css attribute`, () => {
+    cy.get(`input[data-validate-email][data-field="customClasses"]`)
+      .type("abc")
+      .clear();
+
+    cy.get(
+      'div[data-input-validator-target="errors"][data-field="customClasses"]'
+    ).within(() => {
+      cy.get(`div[error="email"]`)
+        .should("exist")
+        .should("not.have.attr", "style");
+      cy.get(`div[error="email"]`)
+        .should("exist")
+        .should("have.attr", "class")
+        .and("include", "text-lg")
+        .and("include", "text-purple-600")
+        .and("include", "font-bold");
+    });
+  });
+});
